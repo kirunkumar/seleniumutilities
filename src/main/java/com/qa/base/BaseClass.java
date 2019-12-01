8ebd8f89720ffa6ea11cf79531f1f7bef1a02d43
kirunkumar <kirun.m@gmail.com>
2019-12-01 06:46:38 -0600

    committing now

--- src/main/java/com/qa/base/BaseClass.java
+++ src/main/java/com/qa/base/BaseClass.java
@@ -1,5 +1,8 @@
 package com.qa.base;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -20,69 +23,83 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import com.qa.utils.Utils;
 
 public class BaseClass extends Utils {
-	
-	public WebDriver driver;
+
+	public WebDriver driver, safdriver;
 	public Properties prop;
 	public FileInputStream fis;
 	public JavascriptExecutor jse;
 	public Logger log;
 	ChromeOptions options;
 	DesiredCapabilities capabilities;
-	
-	public BaseClass(){
-		
+
+	public BaseClass() {
+
 	}
-	
-	public void initializeDriver(){
+
+	public void initializeDriver() {
 		initializeDriver("Chrome");
-		
+
 	}
-		
-	
-	public void initializeDriver(String browserName){
-		
+
+	public void initializeDriver(String browserName) {
+
 		log = Logger.getLogger(getClass());
-		
-		if(browserName.equalsIgnoreCase("Chrome"))
-		{
+
+		if (browserName.equalsIgnoreCase("Chrome")) {
 			options = new ChromeOptions();
 			options.addArguments("--incognito");
+			options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
 			capabilities = DesiredCapabilities.chrome();
 			capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 			
-			if(whichOS() == "mac")
-			{
-				System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
+				
+
+			// WebDriver driver = new RemoteWebDriver(caps);
+
+			if (whichOS() == "mac") {
+				//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
+				System.setProperty("webdriver.chrome.driver", "C:\\Users\\kk\\Documents\\Kiran\\Automation\\drivers\\chromedriver.exe");
 				// /Users/kiran/Documents/Eclipse/Workspace/seleniumutilities/drivers/chromedriver
+			} else
+
+			if (whichOS() == "windows") {
+
+				//System.setProperty("webdriver.chrome.driver",
+					//	System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
+				
+				System.setProperty("webdriver.chrome.driver", "C:\\Users\\kk\\Documents\\Kiran\\Automation\\drivers\\chromedriver.exe");
+
+				// System.setProperty("webdriver.chrome.driver",
+				// "D:\\Software\\Eclipse\\drivers\\chromedriver.exe");
+
+			} else {
+				System.out.println("Browser not listed in this Program");
+				System.exit(0);
+
 			}
-			
-			
-			
+
 			driver = new ChromeDriver();
 		}
-		
-		else {
-			
-				if(browserName.equalsIgnoreCase("Safari"))
-				{
 
-					 driver = new SafariDriver();
-					 					
-				}
-						
+		else {
+
+			if (browserName.equalsIgnoreCase("Safari")) {
+
+				driver = new SafariDriver();
+
+			}
+
 		}
-		
+
 		System.out.println("Test is being executed in " + browserName + " Browser");
-		
-		
-		
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);	
+
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().fullscreen();
-		
+
 	}
-	
+
 	public void loadProperties() {
-		
+
 		try {
 			fis = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/com/qa/data/config.properties");
 			prop = new Properties();
@@ -94,98 +111,95 @@ public class BaseClass extends Utils {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-				
+
 	}
-	
-	public void killBrowserInstance(){
-		
+
+	public void killBrowserInstance() {
+
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		driver.quit();
-		
-		
+
 	}
-	
+
 	public void smallWait() {
-		
+
 		try {
 			Thread.sleep(500);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	public void mediumWait() {
-		
+
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}	
-	
-	 public void openURL(String url) {
-		 
-		 driver.get(url);
-		 waitForLoad(driver);
-		 
-	 }
-	 
-	 
-	 public void openURLHeadLess(String url) {
-		 
-		 log = Logger.getLogger(getClass());
-		 
-			options = new ChromeOptions();
-			options.addArguments("--incognito");
-			options.addArguments("--headless");
-			capabilities = DesiredCapabilities.chrome();
-			capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-			
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
-			driver = new ChromeDriver(options);
-			driver.get(url);
-			
-	 }
-	 
-	 public void waitForLoad(WebDriver driver) {
-		 
-		 initializeJavaScript();
-	        ExpectedCondition<Boolean> pageLoadCondition = new
-	                ExpectedCondition<Boolean>() {
-	                    public Boolean apply(WebDriver driver) {
-	                    	System.out.println("Document State:" + jse.executeScript("return document.readyState"));
-	                        return jse.executeScript("return document.readyState").equals("complete");
-	                    }
-	                };
-	        WebDriverWait wait = new WebDriverWait(driver, 30);
-	        wait.until(pageLoadCondition);
-	  }
 
-	
-	public void initializeJavaScript(){
-		
-		jse = (JavascriptExecutor) driver;
-		
 	}
-	
+
+	public void openURL(String url) {
+
+		driver.get(url);
+		waitForLoad(driver);
+
+	}
+
+	public void openURLHeadLess(String url) {
+
+		log = Logger.getLogger(getClass());
+
+		options = new ChromeOptions();
+		options.addArguments("--incognito");
+		options.addArguments("--headless");
+		options.addArguments("start-maximized");
+		capabilities = DesiredCapabilities.chrome();
+		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
+		driver = new ChromeDriver(options);
+		driver.get(url);
+
+	}
+
+	public void waitForLoad(WebDriver driver) {
+
+		initializeJavaScript();
+		ExpectedCondition<Boolean> pageLoadCondition = new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver driver) {
+				System.out.println("Document State:" + jse.executeScript("return document.readyState"));
+				return jse.executeScript("return document.readyState").equals("complete");
+			}
+		};
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(pageLoadCondition);
+	}
+
+	public void initializeJavaScript() {
+
+		jse = (JavascriptExecutor) driver;
+
+	}
+
 	public void softScrollDown() {
-		  
-	     for(int i=0; i<6000; i++) {
-	    	 
-	    	 jse.executeScript("window.scrollBy(0,1)", "");
-	    	 
-	     }
-	     
+
+		for (int i = 0; i < 6000; i++) {
+
+			jse.executeScript("window.scrollBy(0,1)", "");
+
+		}
+
 	}
 
 }
--- src/test/java/com/daily/webpages/TechPages.java
+++ src/test/java/com/daily/webpages/TechPages.java
@@ -12,69 +12,90 @@ import org.testng.annotations.Test;
 
 import com.qa.base.BaseClass;
 
-public class TechPages extends BaseClass{
-	
-	ArrayList<Object> techLinks = new ArrayList<Object> ();
+public class TechPages extends BaseClass {
 
-	 @Test
-	  public void actualTestHere() {
-		 
-		 WebDriverWait wait = new WebDriverWait(driver, 30);
-		 
-		 
-		//initializeJavaScript();
-		 
-		techLinks.forEach( link -> {
-			
-			try 
-			{
+	ArrayList<Object> techLinks = new ArrayList<Object>();
+
+	@Test(enabled=true)
+	public void actualTestHere() {
+
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+
+		// initializeJavaScript();
+
+		techLinks.forEach(link -> {
+
+			try {
 				System.out.println("Opening URL: " + link.toString());
 				openURL(link.toString());
 				softScrollDown();
-				System.out.println("Done: " + link.toString());	
-				
-			}
-			catch(Exception e) {
+				System.out.println("Done: " + link.toString());
+
+			} catch (Exception e) {
 				System.out.println("****In Exception and continuing with next one**** \n\n");
 				e.printStackTrace();
 			}
-									}
-				
+		}
+
 		);
-		  
-		  
-		  		  
-	  }
-	 
-	  
-	  
-	  @BeforeTest
+
+	}
+	
+	@Test
+	public void secondTest() {
+
+		driver.get("https://cloud.google.com/apigee/");
+		System.out.println("Browser title is:" + driver.getTitle());
+
+	}
+
+	@BeforeTest
 	  public void launchBrowser() {
 		  
 		  initializeDriver("chrome");
 		  
+		  techLinks.add("https://www.theverge.com/");
+		  techLinks.add("https://www.complex.com/music/");
 		  techLinks.add("https://techcrunch.com/");
+		  //techLinks.add("https://www.samsung.com/us/mobile/galaxy-note10/");
 		  techLinks.add("https://www.theverge.com/tech");		  
 		  techLinks.add("https://www.greatandhra.com/index.php");
-		  techLinks.add("https://telugu.oneindia.com/");
 		  techLinks.add("https://www.seleniumhq.org/");
+		  techLinks.add("https://telugu.oneindia.com/");
 		  techLinks.add("https://www.softwaretestingmaterial.com/selenium-interview-questions/");
+		  techLinks.add("https://www.apple.com/mac/");
+		  techLinks.add("https://www.samsung.com/us/mobile/galaxy-note10/");
+		  techLinks.add("https://www.lenovo.com/us/en/d/laptops-by-specs?IPromoID=LEN878502&sort=sortBy&currentResultsLayoutType=grid&visibleDatas=&mode=normal");
+		  techLinks.add("https://www.sakshi.com/");
+		  techLinks.add("https://www.cnn.com/");
+		  techLinks.add("https://cloud.google.com/apigee/");
+		  techLinks.add("https://slickdeals.net/");
 		  
-	  }
-	  
-	  
+		  
 
-	  @AfterTest
-	  public void killBrowser() {
+		
+		//techLinks.add("https://www.skysports.com/football/news");
+		//techLinks.add("https://www.skysports.com/football/news");
+		//techLinks.add("https://www.skysports.com/football/news");
+		//techLinks.add("https://www.skysports.com/football/news");
+		//techLinks.add("https://www.skysports.com/football/news");
+		//techLinks.add("https://www.skysports.com/football/news");
+		//techLinks.add("https://www.skysports.com/football/news");
+		  //techLinks.add("https://www.skysports.com/football/news");
 		  
-		  killBrowserInstance();
+		  
+		  
+		  
+		  //techLinks.add("");
 		  
 		  
 	  }
-	
-	
-	
-	
-	
-	
+
+	@AfterTest
+	public void killBrowser() {
+
+		killBrowserInstance();
+
+	}
+
 }
